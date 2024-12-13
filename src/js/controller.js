50b6631c2ec0b7c253dd463f565eb9b7eb96c39d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-12-12 16:39:46 -0800

    feat:Loading a recipie from the API.

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,3 +7,20 @@ const timeout = function (s) {
         }, s * 1000);
     });
 };
+
+const showRecipes = async function () {
+
+    try {
+        const response = await fetch("https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await response.json();
+        if (!response.ok) throw new Error(`${data.message}  status : ${response.status}`);
+        console.log(response);
+        console.log(data);
+    } catch (err) {
+        debugger;
+        alert("Error fetching recipes");
+        console.log("Error fetching recipes");
+        console.log(err)
+    }
+}
+showRecipes();
