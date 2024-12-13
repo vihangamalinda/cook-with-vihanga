67ae0890a917b4a3f7ab37540a61ccfde4d3350a
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-12-13 13:02:11 -0800

    feat:Adding a spinner.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,6 @@
+import icons from 'url:../img/icons.svg';
+console.log(icons);
+
 const recipeContainer =document.querySelector(".recipe");
 // document.querySelector(".recipe")
 // debugger;
@@ -10,9 +13,24 @@ const timeout = function (s) {
     });
 };
 
+const renderSpinner = function (parentEl) {
+    const markUp = `
+        <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>`
+
+    parentEl.innerHTML='';
+    parentEl.insertAdjacentHTML('afterbegin', markUp);
+}
+
 const showRecipes = async function () {
     const validIngredient = (impureData) =>impureData?impureData:"";
     try {
+        //loading recipe
+        renderSpinner(recipeContainer);
+        // debugger;
         const response = await fetch("https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886");
         const data = await response.json();
         if (!response.ok) throw new Error(`${data.message}  status : ${response.status}`);
@@ -32,7 +50,8 @@ const showRecipes = async function () {
         console.log("recipe V2 modal", recipe);
 
         // 2) Rendering recipes
-        const markup = ` <figure class="recipe__fig">
+        const markup = `
+        <figure class="recipe__fig">
           <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
           <h1 class="recipe__title">
             <span>${recipe.title}</span>
@@ -42,14 +61,14 @@ const showRecipes = async function () {
         <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
+              <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
+              <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text">servings</span>
@@ -57,12 +76,12 @@ const showRecipes = async function () {
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                  <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                  <use href="${icons}#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -70,12 +89,12 @@ const showRecipes = async function () {
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
+              <use href="${icons}#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
             <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark-fill"></use>
             </svg>
           </button>
         </div>
@@ -86,7 +105,7 @@ const showRecipes = async function () {
           ${recipe.ingredients.map(ingredient => {
             return `<li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
+                <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${validIngredient(ingredient.quantity)}</div>
               <div class="recipe__description">
