9dd1a29cf87ae365702e7892e902123c77d66da1
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-12-22 06:45:29 -0800

    feat:Adding a configuration and a helper class.

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,14 +3,6 @@ import recipeView from "./views/recipeView";
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-    return new Promise(function (_, reject) {
-        setTimeout(function () {
-            reject(new Error(`Request took too long! Timeout after ${s} second`));
-        }, s * 1000);
-    });
-};
-
 const controlRecipe = async function () {
     const hashCode = window.location.hash;
     const recipeId = hashCode.slice(1);
--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,16 @@
+import {API_URL} from "./config.js";
+import {getJSON} from "./helpers.js";
+
 export const state = {
     recipe: {},
 }
 
 export const loadRecipe = async (recipeId) => {
     try {
-        const response = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${recipeId}`);
-        const data = await response.json();
-        if (!response.ok) throw new Error(`${data.message}  status : ${response.status}`);
-        console.log(response);
+        // get data from API
+        const data = await getJSON(`${API_URL}/${recipeId}`);
         console.log(data);
+
         const {recipe} = data.data;
         state.recipe = {
             id: recipe.id,
