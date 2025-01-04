bc25960b90f60333d3fb452105c4f8f9d3069eab
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-01-04 13:47:56 -0800

    feat:Adding search related view.

---
+++ src/js/views/searchView.js
@@ -0,0 +1,23 @@
+class SearchView {
+    #parentElement = document.querySelector('.search');
+
+    getQuery() {
+        const query = this.#parentElement.querySelector(".search__field").value;
+        console.log(query);
+        this.#clearInput();
+        return query;
+    }
+
+    #clearInput() {
+        this.#parentElement.querySelector('.search__field').value = '';
+    }
+
+    addHandlerSearch(handler) {
+        this.#parentElement.addEventListener(('submit'), (e) => {
+            e.preventDefault();
+            handler();
+        });
+    }
+}
+
+export default new SearchView();
