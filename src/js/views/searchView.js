b53b9363fed278a292f183c6d9c3939109abf471
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-01-11 08:13:19 -0800

    refactor: Adding protected fields to have consistance through out view layers.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,19 +1,19 @@
 class SearchView {
-    #parentElement = document.querySelector('.search');
+    _parentElement = document.querySelector('.search');
 
     getQuery() {
-        const query = this.#parentElement.querySelector(".search__field").value;
+        const query = this._parentElement.querySelector(".search__field").value;
         console.log(query);
-        this.#clearInput();
+        this._clearInput();
         return query;
     }
 
-    #clearInput() {
-        this.#parentElement.querySelector('.search__field').value = '';
+    _clearInput() {
+        this._parentElement.querySelector('.search__field').value = '';
     }
 
     addHandlerSearch(handler) {
-        this.#parentElement.addEventListener(('submit'), (e) => {
+        this._parentElement.addEventListener(('submit'), (e) => {
             e.preventDefault();
             handler();
         });
