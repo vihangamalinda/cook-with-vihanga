2dca9a1fd9a84aaad2d7b8e379ecf41cdd312d86
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-01-11 21:35:55 -0800

    feat:Adding a view for searched result-data.

---
+++ src/js/views/resultView.js
@@ -0,0 +1,47 @@
+import View from "./View";
+import icons from 'url:../../img/icons.svg';
+
+class ResultView extends View {
+    _parentElement = document.querySelector(".results");
+    _data;
+    _errorMessage = 'No results were found. Please try to search another recipe name. 👩🏾‍🍳';
+
+    render(data) {
+        if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
+
+        this._data = data;
+        const markUp = this._generateMarkup();
+        console.log(data);
+        console.log(this._parentElement);
+        this._clear();
+        this._parentElement.insertAdjacentHTML("afterbegin", markUp);
+    }
+
+    _generateMarkup() {
+        return this._data.map(this.#generateMarkupPreview).join("");
+    }
+
+    #generateMarkupPreview(recipe) {
+        const {id, image, publisher, title} = recipe;
+        return `
+         <li class="preview">
+              <a class="preview__link preview__link--active" href="#${id}">
+                <figure class="preview__fig">
+                  <img src="${image}" alt="${title}" />
+                </figure>
+                <div class="preview__data">
+                  <h4 class="preview__title">${title}</h4>
+                  <p class="preview__publisher">${publisher}</p>
+                  <div class="preview__user-generated">
+                    <svg>
+                      <use href="${icons}#icon-user"></use>
+                    </svg>
+                  </div>
+                </div>
+              </a>
+            </li>`
+    }
+
+}
+
+export default new ResultView();
